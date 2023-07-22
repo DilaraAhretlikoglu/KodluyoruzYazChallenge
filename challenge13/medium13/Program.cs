35fcd8c6b57480a24515d99edfab6415459d9730
Dilara Ahretlikoğlu <97093117+DilaraAhretlikoglu@users.noreply.github.com>
2023-07-22 14:26:22 +0300

    medium

---
+++ challenge13/medium13/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace medium13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Kullanıcıdan bir kelime almanız gerekiyor. Bu kelimenin harflerini büyük harflere dönüştüren bir program yazmanızı istiyorum. 
+        Console.WriteLine("Bir kelime giriniz:");
+        string kelime=Console.ReadLine();
+
+        string newKelime=kelime.ToUpper();
+
+        Console.WriteLine("Orijinal kelime:{0}",kelime);
+        Console.WriteLine("Büyük harfli kelime:{0}:",newKelime);
+    }
+}
