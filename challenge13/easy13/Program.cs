834c9a6d2a07dfee17c8bc31d817f25b9603430f
Dilara Ahretlikoğlu <97093117+DilaraAhretlikoglu@users.noreply.github.com>
2023-07-22 14:24:11 +0300

    easy

---
+++ challenge13/easy13/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace easy13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Kullanıcıdan bir sayı almanızı ve bu sayının asal olup olmadığını kullanıcıya söylemenizi istiyorum. 
+        Console.WriteLine("Bir sayi girin:");
+        int sayi=int.Parse(Console.ReadLine());
+        int sayac=0;
+
+        for (int i=1;i<=sayi;i++)
+        {
+            if(sayi%i==0)
+                sayac++;
+        }
+        if(sayac==2)
+            Console.WriteLine("{0} bir asal sayidir.",sayi);
+        else
+            Console.WriteLine("{0} bir asal sayi degildir.",sayi);
+
+    }
+}
