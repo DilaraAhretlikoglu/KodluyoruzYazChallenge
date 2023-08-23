546e09ba88362fa4f6440e2a809e0aef3722ad5f
Dilara Ahretlikoğlu <97093117+DilaraAhretlikoglu@users.noreply.github.com>
2023-08-23 14:22:27 +0300

    Update Program.cs

--- challenge12/hard12/Program.cs
+++ challenge12/hard12/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        //💪🏻Hard: Bir yüzme havuzunda 2 adet su girişi, 1 adet su çıkışı vardır. İlk su girişi havuzu 10 saatte doldururken, ikinci su girişi havuzu 15 saatte doldurmaktadır. Havuzun kendiliğinden boşalma hızı ise 30 saatte bir doludur. Eğer havuz boşken, her iki su girişi de açılırsa havuz ne kadar sürede dolar?😀
+
         double ilkMusluk = 1.0 / 10.0;   
         double ikinciMusluk = 1.0 / 15.0; 
         double suCikisi = 1.0 / 30.0;  
